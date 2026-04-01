a96e5a5df39c5f6a4ca111967df81689c8aceba2
Vedant Trivedi <vedanttrivedi87@gmail.com>
2026-04-01 21:44:28 +0530

    Clean scalable schema that links a user to a list of sneaker items.

--- backend/src/models/Wishlist.js
+++ backend/src/models/Wishlist.js
@@ -1,1 +1,38 @@
- 
+import mongoose from 'mongoose';
+
+const wishlistItemSchema = new mongoose.Schema(
+  {
+    sneaker: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Sneaker',
+      required: true,
+    },
+    addedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { _id: false }
+);
+
+const wishlistSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+    },
+    items: {
+      type: [wishlistItemSchema],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Wishlist = mongoose.model('Wishlist', wishlistSchema);
+
+export default Wishlist;
